f17b9f2f0ecebd94a6ad907ab1be7bf90bcd1708
Julien Balestra <julien.balestra@gmail.com>
2016-06-28 19:12:28 +0200

    Update docker-entrypoint.sh

--- 5.0/docker-entrypoint.sh
+++ 5.0/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 
 if [ -z $LOGSTASH_CONF ]
 then
@@ -12,6 +13,6 @@ fi
 if [ $AC_APP_NAME ]
 then
 	IP=$(ip addr show eth0 | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
-	grep -c $IP /etc/hosts || echo "$IP $(hostname)" >> /etc/hosts
+	grep -c $IP /etc/hosts || echo "$IP $(hostname)" | tee -a /etc/hosts
 fi
 exec logstash -f $LOGSTASH_CONF
